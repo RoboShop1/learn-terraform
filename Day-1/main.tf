8f56f29f1d10e9cf7aff2eaf30bfd3d9c6173099
chaithanya1812 <bob@example.com>
2024-10-22 17:40:02 +0530

    for_each loop with list

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -4,7 +4,7 @@ variable "list" {
   default = ["one","two","three","four"]
 }
 
-resource "null_resource" "first1" {
+resource "null_resource" "first" {
   for_each = toset(var.list)
   provisioner "local-exec" {
     command = "echo This is - ${base64encode("${each.key}")} and - ${each.value}"
