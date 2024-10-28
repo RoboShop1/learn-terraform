bbe20dedddf9fb44ad0b5989e69a14872fda7c7c
chaithanya1812 <bob@example.com>
2024-10-28 23:10:19 +0530

    terraform practice

--- roboshop-terraform/modules/vpc/main.tf
+++ roboshop-terraform/modules/vpc/main.tf
@@ -42,6 +42,12 @@ resource "aws_route_table" "rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+  }
+
   tags = {
     Name = "${var.env }-public-rt-${count.index}"
   }
