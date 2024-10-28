2a7ac125c13e2a332910e9d6d1f9cea06ef3f4d9
chaithanya1812 <bob@example.com>
2024-10-28 22:18:55 +0530

    terraform practice

--- roboshop-terraform/modules/ec2/main.tf
+++ roboshop-terraform/modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "sg" {
   ingress{
     from_port        = 22
     to_port          = 22
-    protocol         = "ssh"
+    protocol         = "TCP"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
