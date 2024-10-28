b946a650bc54adb091daffd5ef8576bd05105f7a
chaithanya1812 <bob@example.com>
2024-10-28 09:24:56 +0530

    k jobs

---
+++ run.sh
@@ -0,0 +1,21 @@
+#num=(1 2 3 4 5)
+#total=0
+#for i in ${num[@]}
+#do
+#  total=$((total+i))
+#done
+#
+#echo $total
+
+
+name="one two three four"
+
+function one() {
+  IFS="-"
+    echo "-----------"
+    echo "$*"
+    echo "-----------"
+    echo "$@"
+}
+
+one $name
