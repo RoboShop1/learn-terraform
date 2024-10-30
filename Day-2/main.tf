05721c554605a7812dbd67d22df0778f929fa0f6
chaithanya1812 <bob@example.com>
2024-10-30 08:38:58 +0530

    terraform practice

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -4,27 +4,31 @@ resource "aws_instance" "sample" {
   instance_type = "t2.micro"
   availability_zone = "us-east-1a"
 
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
   tags = {
     Name = "demo-${count.index}"
   }
 }
 
 // With list
-
-output "ips" {
-  value = aws_instance.sample[*].public_ip
-}
-
-output "ip-one" {
-  value = element(aws_instance.sample.*.public_ip,0)
-}
-
-output "ip" {
-  value = aws_instance.sample.*.public_ip[0]
-}
-
-output "tags" {
-  value = toset(values({
-    for i, j in aws_instance.sample: i => j.public_ip
-  }))
-}
+#
+#output "ips" {
+#  value = aws_instance.sample[*].public_ip
+#}
+#
+#output "ip-one" {
+#  value = element(aws_instance.sample.*.public_ip,0)
+#}
+#
+#output "ip" {
+#  value = aws_instance.sample.*.public_ip[0]
+#}
+#
+#output "tags" {
+#  value = toset(values({
+#    for i, j in aws_instance.sample: i => j.public_ip
+#  }))
+#}
