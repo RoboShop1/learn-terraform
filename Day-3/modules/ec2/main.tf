09ba0f1901de879ed076e3c48f20b23688daecc3
chaithanya1812 <bob@example.com>
2024-11-01 13:28:41 +0530

    terraform practice

--- Day-3/modules/ec2/main.tf
+++ Day-3/modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "main" {
     ami           = "ami-0ddc798b3f1a5117e"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
     Name = var.name
@@ -12,4 +13,24 @@ variable "name" {}
 
 output "ec2" {
   value = aws_instance.main
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.name}-sg"
+  description = "Allow ssh"
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
