8e7357c9442366db54053b439bfdea6f2053ae49
chaithanya1812 <bob@example.com>
2024-11-03 16:48:34 +0530

    roboshop-v1

--- Day-4/utility-project/main.tf
+++ Day-4/utility-project/main.tf
@@ -17,5 +17,5 @@ data "aws_ec2_instance_type_offering" "example" {
 }
 
 output "name" {
-  value = data.aws_ec2_instance_type_offering.example
+  value = data.aws_ec2_instance_type_offering.example.instance_type
 }
