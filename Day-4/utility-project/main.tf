d0ff4d08cf4049fed5ee6d2c0ad68d99e9e88265
chaithanya1812 <bob@example.com>
2024-11-03 17:50:30 +0530

    roboshop-v1

--- Day-4/utility-project/main.tf
+++ Day-4/utility-project/main.tf
@@ -1,31 +1,36 @@
-data "aws_availability_zones" "zones" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
-#output "zones" {
-#  value = data.aws_availability_zones.zones.names
+#data "aws_availability_zones" "zones" {
+#  filter {
+#    name   = "opt-in-status"
+#    values = ["opt-in-not-required"]
+#  }
+#}
+#
+##output "zones" {
+##  value = data.aws_availability_zones.zones.names
+##}
+#
+#data "aws_ec2_instance_type_offerings" "example" {
+#  for_each = toset(data.aws_availability_zones.zones.names)
+#
+#  filter {
+#    name   = "instance-type"
+#    values = ["t3,micro"]
+#  }
+#
+#  filter {
+#    name   = "location"
+#    values = [each.value]
+#  }
+#
+#  location_type = "availability-zone-id"
+#}
+#
+#output "instance_types" {
+#  value = keys(data.aws_ec2_instance_type_offerings.example)
 #}
 
-data "aws_ec2_instance_type_offerings" "example" {
-  for_each = toset(data.aws_availability_zones.zones.names)
 
-  filter {
-    name   = "instance-type"
-    values = ["t3,micro"]
-  }
-
-  filter {
-    name   = "location"
-    values = [each.value]
-  }
-
-  location_type = "availability-zone-id"
+resource "aws_instance" "sample" {
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t3.micro"
 }
-
-output "instance_types" {
-  value = keys(data.aws_ec2_instance_type_offerings.example)
-}
-
