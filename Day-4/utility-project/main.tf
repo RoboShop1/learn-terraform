76b2125b6eb65f4bcfd0ac023420c4f6563b98e0
chaithanya1812 <bob@example.com>
2024-11-03 17:24:44 +0530

    roboshop-v1

--- Day-4/utility-project/main.tf
+++ Day-4/utility-project/main.tf
@@ -14,6 +14,10 @@ data "aws_ec2_instance_type_offering" "example" {
     name   = "instance-type"
     values = ["t2.micro"]
   }
+  filter {
+    name   = "location"
+    values = ["us-east-1a"]
+  }
   location_type = "availability-zone"
 }
 
