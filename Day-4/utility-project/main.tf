9e57f09b4bde728d43cc23929810a96f49acc33a
chaithanya1812 <bob@example.com>
2024-11-03 17:37:00 +0530

    roboshop-v1

--- Day-4/utility-project/main.tf
+++ Day-4/utility-project/main.tf
@@ -1,20 +1,31 @@
-#data "aws_availability_zones" "example" {
-#  filter {
-#    name   = "opt-in-status"
-#    values = ["opt-in-not-required"]
-#  }
-#}
-#
-#output "zones" {
-#  value = data.aws_availability_zones.example.names
-#}
+data "aws_availability_zones" "zones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
+output "zones" {
+  value = data.aws_availability_zones.zones.names
+}
+/*
 data "aws_ec2_instance_type_offerings" "example" {
+  for_each = toset(data.aws_availability_zones.example.names)
   filter {
     name   = "instance-type"
     values = ["t2.micro"]
   }
 
-  location_type = "availability-zone"
+  filter {
+    name   = "location"
+    values = [each.value]
+  }
+
+  location_type = "availability-zone-id"
 }
 
+output "names" {
+  value =
+}
+
+*/
