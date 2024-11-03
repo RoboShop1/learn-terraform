932e970e13c975add3f6c0e2fc2c9ac615ce662c
chaithanya1812 <bob@example.com>
2024-11-03 16:44:31 +0530

    roboshop-v1

--- Day-4/utility-project/main.tf
+++ Day-4/utility-project/main.tf
@@ -1,14 +1,19 @@
-provider "aws" {
-  region = "ap-south-1"
+#data "aws_availability_zones" "example" {
+#  filter {
+#    name   = "opt-in-status"
+#    values = ["opt-in-not-required"]
+#  }
+#}
+#
+#output "zones" {
+#  value = data.aws_availability_zones.example.names
+#}
+
+
+data "aws_ec2_instance_type" "example" {
+  instance_type = "t2.micro"
 }
 
-data "aws_availability_zones" "example" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
-output "zones" {
-  value = data.aws_availability_zones.example.names
+output "name" {
+  value = data.aws_ec2_instance_type.example
 }
