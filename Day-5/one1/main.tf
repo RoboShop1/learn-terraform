54c1b9d02601f90d231da5f4bea67828ea4a43f4
chaithanya1812 <bob@example.com>
2024-11-06 22:34:21 +0530

    roboshop-v1

--- Day-5/one1/main.tf
+++ Day-5/one1/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "main" {
   ami           = "ami-0ddc798b3f1a5117e"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   availability_zone = "us-east-1c"
 
   lifecycle {
