a3a3961d03af6a09a1950adcdb41bcebdbbdc5be
chaithanya1812 <bob@example.com>
2024-11-07 20:02:55 +0530

    roboshop-v1

--- Day-5/ec2/main.tf
+++ Day-5/ec2/main.tf
@@ -3,11 +3,33 @@ resource "aws_instance" "main" {
   instance_type = "t2.micro"
   subnet_id     = var.subnet_id
   key_name      = "nvirginia"
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
     Name = "${var.name}"
   }
 }
 
+resource "aws_security_group" "main" {
+  ingress {
+    from_port        = var.app_ports
+    to_port          = var.app_ports
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags ={
+    Name = "${element(split("-",var.name),0)}-sg"
+  }
+}
+
 variable "name" {}
 variable "subnet_id" {}
+variable "app_ports" {}
