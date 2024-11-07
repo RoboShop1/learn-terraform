9c18029624ff3d47d785f3449b027f2e8c0b1f3b
chaithanya1812 <bob@example.com>
2024-11-07 14:34:43 +0530

    roboshop-v1

--- Day-5/vpc/main.tf
+++ Day-5/vpc/main.tf
@@ -61,6 +61,14 @@ resource "aws_subnet" "db_subnets" {
 }
 
 
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
 
 
 
