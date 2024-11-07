d706ccb90d61596a80499410ff83a02661c71b26
chaithanya1812 <bob@example.com>
2024-11-07 10:35:50 +0530

    roboshop-v1

--- Day-5/vpc/main.tf
+++ Day-5/vpc/main.tf
@@ -1,18 +1,15 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 
-  tags = {
-    Name = merge({
+  tags = merge({
       Name = "${var.env}-vpc"
     },local.common_tags)
-  }
 }
+
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = merge({
+  tags = merge({
       Name = "${var.env}-igw"
     },local.common_tags)
-  }
 }
