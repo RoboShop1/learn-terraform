ba0c9a9a37fba285b7a73cd0e0d7e54be5ed7bbe
chaithanya1812 <bob@example.com>
2024-11-07 11:01:50 +0530

    roboshop-v1

--- Day-5/vpc/main.tf
+++ Day-5/vpc/main.tf
@@ -26,3 +26,12 @@ resource "aws_subnet" "public_subnets" {
   },local.common_tags)
 }
 
+
+resource "aws_security_group_rule" "example" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = "0.0.0.0/0"
+  security_group_id = aws_vpc.main.default_security_group_id
+}
