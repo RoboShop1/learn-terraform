0566cfc7637737c1fe07b4040853eff355d89d5b
chaithanya1812 <bob@example.com>
2024-11-07 15:06:37 +0530

    roboshop-v1

--- Day-5/vpc/main.tf
+++ Day-5/vpc/main.tf
@@ -75,6 +75,10 @@ resource "aws_route_table_association" "public-rta" {
 resource "aws_route_table" "common-rt" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0."
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
   tags = {
     Name = "common-rt"
   }
