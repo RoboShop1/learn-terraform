24f075b84e57ecd31cb8e0e58fa4f0d9959f7090
chaithanya1812 <bob@example.com>
2024-11-07 14:44:38 +0530

    roboshop-v1

--- Day-5/vpc/main.tf
+++ Day-5/vpc/main.tf
@@ -13,11 +13,7 @@ resource "aws_internet_gateway" "gw" {
     },local.common_tags)
 }
 
-resource "aws_route" "demo" {
-  route_table_id = aws_vpc.main.default_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.gw.id
-}
+
 
 # //////////////////
 # Public-subnets ////
