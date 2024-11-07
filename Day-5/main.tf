83ed00757c62def0d57c8a216f9ba6ec97a3545a
chaithanya1812 <bob@example.com>
2024-11-07 11:20:26 +0530

    roboshop-v1

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -5,3 +5,7 @@ module "vpc" {
   availability_zone = ["us-east-1a","us-east-1b","us-east-1c"]
   public_subnets_cidr = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
 }
+
+output "all" {
+  value = module.vpc.rt
+}
