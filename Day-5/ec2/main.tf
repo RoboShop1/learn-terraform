230288b17d368b922c68ebebf02c59dd378b1d59
chaithanya1812 <bob@example.com>
2024-11-07 19:53:21 +0530

    roboshop-v1

--- Day-5/ec2/main.tf
+++ Day-5/ec2/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "main" {
   ami           = "ami-0ddc798b3f1a5117e"
   instance_type = "t2.micro"
   subnet_id     = var.subnet_id
+  key_name      = "nvirginia"
 
   tags = {
     Name = "${var.name}"
