a3a2fa019cd13a439ba69f1beb29cd1f4d08c069
chaithanya1812 <bob@example.com>
2024-11-08 10:24:40 +0530

    roboshop-v1

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -27,7 +27,7 @@ variable "subnets" {
       availability_zone = ["us-east-1a","us-east-1b"]
     }
     web = {
-      cidr_block        = ["10.0.1.0/24","10.0.2.0/24"]
+      cidr_block        = ["10.0.3.0/24","10.0.4.0/24"]
       availability_zone = ["us-east-1a","us-east-1b"]
     }
   }
