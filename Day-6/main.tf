19b323426d62ff77ab4e24f7a9ba684bb21c1677
chaithanya1812 <bob@example.com>
2024-11-08 10:21:34 +0530

    roboshop-v1

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -9,26 +9,26 @@ output "main" {
   value = module.vpc
 }
 
-#module "subnets" {
-#  source = "./subnets"
-#  for_each = var.subnets
-#
-#  name              = each.key
-#  vpc_id            = module.vpc.vpc_id
-#  cidr_block        = each.value["cidr_block"]
-#  availability_zone = each.value["availability_zone"]
-#}
-#
-#
-#variable "subnets" {
-#  default = {
-#    public = {
-#      cidr_block        = ["10.0.1.0/24","10.0.1.0/24"]
-#      availability_zone = ["us-east-1a","us-east-1b"]
-#    }
-#    web = {
-#      cidr_block        = ["10.0.1.0/24","10.0.1.0/24"]
-#      availability_zone = ["us-east-1a","us-east-1b"]
-#    }
-#  }
+module "subnets" {
+  source = "./subnets"
+  for_each = var.subnets
+
+  name              = each.key
+  vpc_id            = module.vpc["dev"].vpc_id
+  cidr_block        = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+}
+
+
+variable "subnets" {
+  default = {
+    public = {
+      cidr_block        = ["10.0.1.0/24","10.0.1.0/24"]
+      availability_zone = ["us-east-1a","us-east-1b"]
+    }
+    web = {
+      cidr_block        = ["10.0.1.0/24","10.0.1.0/24"]
+      availability_zone = ["us-east-1a","us-east-1b"]
+    }
+  }
 #}
