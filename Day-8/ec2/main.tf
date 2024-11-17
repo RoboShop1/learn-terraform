7eff3392e2352659d688142fbef4e169a5448431
chaithanya1812 <bob@example.com>
2024-11-17 11:23:18 +0530

    vpc transit gate-way

--- Day-8/ec2/main.tf
+++ Day-8/ec2/main.tf
@@ -4,8 +4,14 @@ resource "aws_ec2_transit_gateway" "transit" {
   }
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc" {
-  subnet_ids         = ["subnet-0e7abd07665847a17","subnet-020e399f2d3bf0e95","subnet-08ac43eb519942608","subnet-0b508cd091db5da8e"]
+resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-private" {
+  subnet_ids         = ["subnet-0e7abd07665847a17","subnet-020e399f2d3bf0e95"]
+  transit_gateway_id = aws_ec2_transit_gateway.transit.id
+  vpc_id             = "vpc-0cdf0504e5f63fd78"
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-public" {
+  subnet_ids         = ["subnet-08ac43eb519942608","subnet-0b508cd091db5da8e"]
   transit_gateway_id = aws_ec2_transit_gateway.transit.id
   vpc_id             = "vpc-0cdf0504e5f63fd78"
 }
