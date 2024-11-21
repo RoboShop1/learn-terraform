9429fbc5fe4bc00f8e595db1794550c8a05ce96c
chaithanya1812 <bob@example.com>
2024-11-21 09:21:51 +0530

    aws eks module

--- k8s-terraform/eks/external-secrets.tf
+++ k8s-terraform/eks/external-secrets.tf
@@ -1,16 +1,16 @@
-resource "helm_release" "external-secrets1" {
-  depends_on = [null_resource.get-config]
-  name             = "external-secrets"
-  repository       = "https://charts.external-secrets.io"
-  chart            = "external-secrets"
-  namespace         = "external-secrets"
-  create_namespace = true
-  version = "main"
-}
+#resource "helm_release" "external-secrets1" {
+#  depends_on = [null_resource.get-config]
+#  name             = "external-secrets"
+#  repository       = "https://charts.external-secrets.io"
+#  chart            = "external-secrets"
+#  namespace         = "external-secrets"
+#  create_namespace = true
+#  version = "main"
+#}
 
 
 resource "kubernetes_manifest" "secret-vault-token" {
-  depends_on = [helm_release.external-secrets1]
+  // depends_on = [helm_release.external-secrets1]
   manifest = {
     "apiVersion" = "v1"
     "kind"       = "Secret"
@@ -28,7 +28,7 @@ variable "vault_ip" {
   default = "54.234.229.240"
 }
 resource "kubernetes_manifest" "cluster-secret-store" {
-  depends_on = [helm_release.external-secrets1]
+ // depends_on = [helm_release.external-secrets1]
 
   manifest = {
     "apiVersion": "external-secrets.io/v1beta1",
