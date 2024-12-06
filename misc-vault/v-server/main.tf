0ec5624fced5ba1b31424e9c852e696733b8e983
chaithanya <chaithanya1812@gmail.com>
2024-12-07 01:12:43 +0530

    sample werf

---
+++ misc-vault/v-server/main.tf
@@ -0,0 +1,41 @@
+
+resource "aws_security_group" "vault-sg" {
+  name = "vault-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "vault-sg"
+  }
+}
+
+resource "aws_security_group_rule" "ingres-rule" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = [0.0.0.0/0]
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0665a56c7cd09a0e0"]
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type = "persistent"
+    }
+  }
+  tags = {
+    Name = "vault-server"
+  }
+}
