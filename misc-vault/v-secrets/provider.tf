bfa229ebfe70d0557f1967d3a0a1ca2d19f95ac0
chaithanya <chaithanya1812@gmail.com>
2024-12-07 02:16:54 +0530

    sample werf

--- misc-vault/v-secrets/provider.tf
+++ misc-vault/v-secrets/provider.tf
@@ -1,7 +1,7 @@
-# provider "vault" {
-#   address = "http://172.31.33.80:8200"
-#   token   = var.token
-#   skip_tls_verify = true
-# }
-#
-# variable "token" {}
+provider "vault" {
+  address = "http://172.31.33.80:8200"
+  token   = var.token
+  skip_tls_verify = true
+}
+
+variable "token" {}
