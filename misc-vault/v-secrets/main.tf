a54a729ec3ed994a27350078f24e049677068bee
chaithanya <chaithanya1812@gmail.com>
2024-12-07 02:43:26 +0530

    sample werf

--- misc-vault/v-secrets/main.tf
+++ misc-vault/v-secrets/main.tf
@@ -23,23 +23,22 @@ variable "services" {
 
 
 resource "vault_mount" "app_mount" {
-  for_each    = var.services
-  path        = each.key
+  path        = "roboshop-dev"
   type        = "kv"
   options     = { version = "2" }
-  description = "KV Version 2 Roboshop ${each.key}"
+  description = "KV Version 2 Roboshop"
 }
 
 output "all" {
   value = vault_mount.app_mount
 }
 resource "vault_kv_secret_v2" "env-dev" {
-  for_each                   = vault_mount.app_mount
+  for_each                   = var.services
 
-  mount                      = each.key
-  name                       = "dev-env"
+  mount                      = vault_mount.app_mount.path
+  name                       = each.key
   cas                        = 1
   delete_all_versions        = true
-  data_json                  = jsonencode(lookup(lookup(var.services,"${each.key}","null"),"dev-env","null"))
+  data_json                  = jsonencode(lookup(each.value,["dev-env"],"null"))
 
 }
