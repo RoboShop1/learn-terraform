ea1c86ab7b10af08dcfa0e204c77a3bf5f75a150
chaithanya <chaithanya1812@gmail.com>
2024-12-07 02:16:34 +0530

    sample werf

--- misc-vault/v-secrets/main.tf
+++ misc-vault/v-secrets/main.tf
@@ -2,97 +2,47 @@ variable "services" {
   default = {
     frontend = {
       dev-env = {
-       app = "dev"
+        app = "dev"
       }
       prod-env = {
-       app = "prod"
+        app = "prod"
       }
-
     }
     catalogue = {
       dev-env = {
-
+        app = "dev"
       }
       prod-env = {
-
+        app = "prod"
       }
-
-
-    }
-    cart = {
-      dev-env = {
-
-      }
-      prod-env = {
-
-      }
-
-
-    }
-    user = {
-      dev-env = {
-
-      }
-      prod-env = {
-
-      }
-
-    }
-    shipping = {
-      dev-env = {
-
-      }
-      prod-env = {
-
-      }
-
-
-    }
-    payment = {
-      dev-env = {
-
-      }
-      prod-env = {
-
-      }
-
     }
   }
 }
 
 
-locals {
-  keys = { for k,v in var.services: keys(k) => values(v) }
+
+
+resource "vault_mount" "app_mount" {
+  for_each = var.services
+  path        = each.key
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 Roboshop ${each.key}"
 }
 
-output "demo" {
-  value = local.keys
+
+resource "vault_kv_secret_v2" "env-dev" {
+  for_each                   = var.services
+  mount                      = vault_mount.app_mount.path
+  name                       = "env-dev"
+  cas                        = 1
+  delete_all_versions        = true
+  data_json                  = jsonencode(each.value["env-dev"])
+  custom_metadata {
+    max_versions = 5
+    data = {
+      foo = "vault@example.com",
+      bar = "12345"
+    }
+  }
 }
-# resource "vault_mount" "app_mount" {
-#   for_each = var.services
-#   path        = each.key
-#   type        = "kv"
-#   options     = { version = "2" }
-#   description = "KV Version 2 Roboshop ${each.key}"
-# }
-#
-#
-# resource "vault_kv_secret_v2" "example" {
-#   mount                      = vault_mount.app_mount.path
-#   name                       = ""
-#   cas                        = 1
-#   delete_all_versions        = true
-#   data_json                  = jsonencode(
-#     {
-#       zip       = "zap",
-#       foo       = "bar"
-#     }
-#   )
-#   custom_metadata {
-#     max_versions = 5
-#     data = {
-#       foo = "vault@example.com",
-#       bar = "12345"
-#     }
-#   }
-# }
