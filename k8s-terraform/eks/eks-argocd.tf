a4d5b9a08f2ab1cbbd72502e53bc25a7394c6239
chaithanya <chaithanya1812@gmail.com>
2024-12-07 13:03:58 +0530

    sample werf

--- k8s-terraform/eks/eks-argocd.tf
+++ k8s-terraform/eks/eks-argocd.tf
@@ -1,17 +1,17 @@
 
-# resource "null_resource" "install-argocd" {
-#  depends_on = [null_resource.get-config,aws_eks_node_group.dev-eks-public-nodegroup]
-#
-#  provisioner "local-exec" {
-#    command = <<EOT
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-#
-# sleep 20
-# kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-# EOT
-#  }
-# }
+resource "null_resource" "install-argocd" {
+ depends_on = [null_resource.get-config,aws_eks_node_group.dev-eks-public-nodegroup]
+
+ provisioner "local-exec" {
+   command = <<EOT
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+sleep 20
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+EOT
+ }
+}
 
 #resource "kubernetes_annotations" "example" {
 #  depends_on = [null_resource.install-argocd]
