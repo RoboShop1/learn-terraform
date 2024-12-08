9556d306f028a93272a2548b715ab0273e3b7d90
chaithanya <chaithanya1812@gmail.com>
2024-12-08 23:03:04 +0530

    sample werf

--- Day-10/main.tf
+++ Day-10/main.tf
@@ -1,8 +1,8 @@
 data "aws_iam_policy_document" "example" {
   statement {
-      Sid = "VisualEditor0",
-      Effect =  "Allow",
-      Action = "s3:*",
+      Sid = "VisualEditor0"
+      Effect =  "Allow"
+      Action = "s3:*"
       Resource =  "*"
     }
 }
