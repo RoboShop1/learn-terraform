20cc47114c0f4fdca62b00cc9976115ed17d5847
chaithanya <chaithanya1812@gmail.com>
2024-12-08 22:59:16 +0530

    sample werf

--- Day-10/main.tf
+++ Day-10/main.tf
@@ -1,3 +1,15 @@
-module "sample" {
-  source = ""
+data "aws_iam_policy_document" "example" {
+  Statement
+    {
+      Sid: "VisualEditor0",
+      Effect: "Allow",
+      Action: "s3:*",
+      Resource: "*"
+    }
+}
+
+resource "aws_iam_policy" "example" {
+  name   = "example_policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.example.json
 }
