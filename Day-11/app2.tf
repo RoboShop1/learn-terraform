aa5e3cb391651bfe14c8ee8db271fb1acb150370
chaithanya <chaithanya1812@gmail.com>
2024-12-13 11:28:16 +0530

    sample werf1

---
+++ Day-11/app2.tf
@@ -0,0 +1,49 @@
+resource "kubernetes_manifest" "app2-pod" {
+  manifest = {
+    "apiVersion": "v1",
+    "kind": "Pod",
+    "metadata": {
+      "name": "app2",
+      "namespace": "default"
+      "labels": {
+        "nginx": "app2"
+      }
+    },
+    "spec": {
+      "containers": [
+        {
+          "name": "app2",
+          "image": "chaitu1812/aws-alb",
+          "ports": [
+            {
+              "containerPort": 80
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "app2-service" {
+  manifest ={
+    "apiVersion": "v1",
+    "kind": "Service",
+    "metadata": {
+      "name": "app2-service",
+      "namespace": "default"
+    },
+    "spec": {
+      "selector": {
+        "nginx": "app2"
+      },
+      "ports": [
+        {
+          "protocol": "TCP",
+          "port": 80,
+          "targetPort": 80
+        }
+      ]
+    }
+  }
+}
---
+++ Day-11/app3.tf
@@ -0,0 +1,49 @@
+resource "kubernetes_manifest" "app3-pod" {
+  manifest = {
+    "apiVersion": "v1",
+    "kind": "Pod",
+    "metadata": {
+      "name": "app3",
+      "namespace": "default"
+      "labels": {
+        "nginx": "app3"
+      }
+    },
+    "spec": {
+      "containers": [
+        {
+          "name": "app3",
+          "image": "chaitu1812/aws-alb",
+          "ports": [
+            {
+              "containerPort": 80
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "app3-service" {
+  manifest ={
+    "apiVersion": "v1",
+    "kind": "Service",
+    "metadata": {
+      "name": "app3-service",
+      "namespace": "default"
+    },
+    "spec": {
+      "selector": {
+        "nginx": "app3"
+      },
+      "ports": [
+        {
+          "protocol": "TCP",
+          "port": 80,
+          "targetPort": 80
+        }
+      ]
+    }
+  }
+}
