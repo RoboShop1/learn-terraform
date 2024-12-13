20e49fdba9f5461a2f779f936fc29bc065da9267
chaithanya <chaithanya1812@gmail.com>
2024-12-13 11:23:26 +0530

    sample werf1

---
+++ Day-11/main.tf
@@ -0,0 +1,52 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+
+}
+
+resource "kubernetes_manifest" "app1-pod" {
+  manifest = {
+    "apiVersion": "v1",
+    "kind": "Pod",
+    "metadata": {
+      "name": "app1",
+      "labels": {
+        "nginx": "app1"
+      }
+    },
+    "spec": {
+      "containers": [
+        {
+          "name": "app1",
+          "image": "chaitu1812/aws-alb",
+          "ports": [
+            {
+              "containerPort": 80
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "app1-service" {
+  manifest ={
+    "apiVersion": "v1",
+    "kind": "Service",
+    "metadata": {
+      "name": "app1-service"
+    },
+    "spec": {
+      "selector": {
+        "nginx": "app1"
+      },
+      "ports": [
+        {
+          "protocol": "TCP",
+          "port": 80,
+          "targetPort": 80
+        }
+      ]
+    }
+  }
+}
