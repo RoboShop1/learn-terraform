476052c8ab4580b675b7629d787ae20d26405e52
chaithanya <chaithanya1812@gmail.com>
2024-12-27 12:22:22 +0530

    sample werf1

--- Day-34/transit-gateway/main.tf
+++ Day-34/transit-gateway/main.tf
@@ -33,15 +33,15 @@ variable "dev_subnets" {
 #   tags = {
 #     Name  = "dev-vpc-attachment1"
 #   }
-# }
+#}
 
 
-# resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-attach" {
-#   transit_gateway_id = aws_ec2_transit_gateway.dev-to-prod.id
-#   vpc_id             = "vpc-0878f6ef79e3c3551"
-#   subnet_ids         = ["subnet-0a02de54972fc07ce","subnet-09f63215a7fb78292"]
-#
-#   tags = {
-#     Name = "dev-tgw-vpc-attachment"
-#   }
-# }
+resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-attach" {
+  transit_gateway_id = aws_ec2_transit_gateway.dev-to-prod.id
+  vpc_id             = "vpc-0878f6ef79e3c3551"
+  subnet_ids         = ["subnet-0a02de54972fc07ce","subnet-09f63215a7fb78292"]
+
+  tags = {
+    Name = "dev-tgw-vpc-attachment"
+  }
+}
