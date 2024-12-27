9ced9e85c702813bbf6ee0347721f8f2d95cdcfb
chaithanya <chaithanya1812@gmail.com>
2024-12-27 12:24:59 +0530

    sample werf1

--- Day-34/transit-gateway/main.tf
+++ Day-34/transit-gateway/main.tf
@@ -12,29 +12,6 @@ variable "dev_subnets" {
 
 
 
-# resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-attach1" {
-#
-#   subnet_ids         = [var.dev_subnets[0]]
-#   transit_gateway_id = aws_ec2_transit_gateway.dev-to-prod.id
-#   vpc_id             = "vpc-0878f6ef79e3c3551"
-#
-#   tags = {
-#     Name  = "dev-vpc-attachment0"
-#   }
-# }
-#
-#
-# resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-attach2" {
-#
-#   subnet_ids         = [var.dev_subnets[1]]
-#   transit_gateway_id = aws_ec2_transit_gateway.dev-to-prod.id
-#   vpc_id             = "vpc-0878f6ef79e3c3551"
-#
-#   tags = {
-#     Name  = "dev-vpc-attachment1"
-#   }
-#}
-
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-attach" {
   transit_gateway_id = aws_ec2_transit_gateway.dev-to-prod.id
@@ -45,3 +22,17 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-attach" {
     Name = "dev-tgw-vpc-attachment"
   }
 }
+
+
+//  prod
+
+
+# resource "aws_ec2_transit_gateway_vpc_attachment" "prod-vpc-attach" {
+#   transit_gateway_id = aws_ec2_transit_gateway.dev-to-prod.id
+#   vpc_id             = "vpc-0878f6ef79e3c3551"
+#   subnet_ids         = ["subnet-0a02de54972fc07ce","subnet-09f63215a7fb78292"]
+#
+#   tags = {
+#     Name = "dev-tgw-vpc-attachment"
+#   }
+# }
