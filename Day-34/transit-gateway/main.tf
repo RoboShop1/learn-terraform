6cb01c9675bfe7f113233e19e20e522c6480a2b6
chaithanya <chaithanya1812@gmail.com>
2024-12-27 12:06:15 +0530

    sample werf1

--- Day-34/transit-gateway/main.tf
+++ Day-34/transit-gateway/main.tf
@@ -14,5 +14,9 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "dev-vpc-attach" {
   subnet_ids         = [var.dev_subnets]
   transit_gateway_id = aws_ec2_transit_gateway.dev-to-prod.id
   vpc_id             = "vpc-0878f6ef79e3c3551"
+
+  tags = {
+    Name  = "dev-vpc-attachment"
+  }
 }
 
