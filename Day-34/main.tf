cb17bf7639da0ab0d23776c0df4853e1860dedfe
chaithanya <chaithanya1812@gmail.com>
2024-12-27 11:00:28 +0530

    sample werf1

--- Day-34/main.tf
+++ Day-34/main.tf
@@ -3,10 +3,10 @@ module "dev-vpc" {
   env                       = "dev"
   vpc_cidr_block            = "10.1.0.0/16"
 
-  public_subnet_cidr_blocks = ["10.1.1.0/16","10.1.2.0/16"]
-  web_subnet_cidr_blocks    = ["10.1.3.0/16","10.1.4.0/16"]
-  app_subnet_cidr_blocks    = ["10.1.5.0/16","10.1.6.0/16"]
-  db_subnet_cidr_blocks     = ["10.1.7.0/16","10.1.8.0/16"]
+  public_subnet_cidr_blocks = ["10.1.1.0/24","10.1.2.0/24"]
+  web_subnet_cidr_blocks    = ["10.1.3.0/24","10.1.4.0/24"]
+  app_subnet_cidr_blocks    = ["10.1.5.0/24","10.1.6.0/24"]
+  db_subnet_cidr_blocks     = ["10.1.7.0/24","10.1.8.0/24"]
 
   public_availability_zones = ["us-east-1a", "us-east-1b"]
   web_availability_zones    = ["us-east-1a", "us-east-1b"]
