044680baaf1737c98c943f1d62370ca04e25b3a1
chaithanya <chaithanya1812@gmail.com>
2025-01-01 12:40:59 +0530

    sample werf1

---
+++ k8s-terraform/eks/ebs-manual.tf
@@ -0,0 +1,37 @@
+resource "aws_eks_addon" "ebs-csi-addon" {
+  depends_on = [
+    aws_iam_role.ebs-csi-role,
+    aws_iam_role_policy_attachment.ebs-policy-attach
+  ]
+  cluster_name                = aws_eks_cluster.dev-eks.name
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = "v1.37.0-eksbuild.1"
+  resolve_conflicts_on_create = "OVERWRITE"
+  service_account_role_arn    = aws_iam_role.ebs-csi-role.arn
+}
+
+
+resource "aws_iam_role" "ebs-csi-role" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs-policy-attach" {
+  role       = aws_iam_role.ebs-csi-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+
