57d143cb3d3bc019f68895708009f1509bd3479a
chaithanya <chaithanya1812@gmail.com>
2025-01-01 13:05:28 +0530

    sample werf1

--- k8s-terraform/eks/ebs-manual.tf
+++ k8s-terraform/eks/ebs-manual.tf
@@ -1,15 +1,15 @@
-resource "aws_eks_addon" "ebs-csi-addon" {
-  depends_on = [
-    aws_iam_role.ebs-csi-role,
-    aws_iam_role_policy_attachment.ebs-policy-attach
-  ]
-  cluster_name                = aws_eks_cluster.dev-eks.name
-  addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.37.0-eksbuild.1"
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-  service_account_role_arn    = aws_iam_role.ebs-csi-role.arn
-}
+# resource "aws_eks_addon" "ebs-csi-addon" {
+#   depends_on = [
+#     aws_iam_role.ebs-csi-role,
+#     aws_iam_role_policy_attachment.ebs-policy-attach
+#   ]
+#   cluster_name                = aws_eks_cluster.dev-eks.name
+#   addon_name                  = "aws-ebs-csi-driver"
+#   addon_version               = "v1.37.0-eksbuild.1"
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   resolve_conflicts_on_update = "OVERWRITE"
+#   service_account_role_arn    = aws_iam_role.ebs-csi-role.arn
+# }
 
 
 
