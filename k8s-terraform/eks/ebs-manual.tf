e339575b3d09f2321cd89ef7bfb04297360fe36c
chaithanya <chaithanya1812@gmail.com>
2025-01-01 12:49:16 +0530

    sample werf1

--- k8s-terraform/eks/ebs-manual.tf
+++ k8s-terraform/eks/ebs-manual.tf
@@ -7,30 +7,54 @@ resource "aws_eks_addon" "ebs-csi-addon" {
   addon_name                  = "aws-ebs-csi-driver"
   addon_version               = "v1.37.0-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
   service_account_role_arn    = aws_iam_role.ebs-csi-role.arn
 }
 
 
-resource "aws_iam_role" "ebs-csi-role" {
-  name = "test_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
+data "aws_iam_policy_document" "eks-route-assume-role" {
+ statement {
+   effect = "Allow"
+
+   principals {
+     type        = "Service"
+     identifiers = ["pods.eks.amazonaws.com"]
+   }
+
+   actions = [
+     "sts:AssumeRole",
+     "sts:TagSession"
+   ]
+ }
 }
 
+resource "aws_iam_role" "ebs-csi-role" {
+ name               = "aws_elb_role"
+ assume_role_policy = data.aws_iam_policy_document.eks-route-assume-role.json
+}
+
+
+# resource "aws_iam_role" "ebs-csi-role" {
+#   name = "test_role"
+#
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "eks.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+# }
+
 resource "aws_iam_role_policy_attachment" "ebs-policy-attach" {
-  role       = aws_iam_role.ebs-csi-role.name
+  role       = aws_iam_role.eks-csi-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
