c4f99bd2d14657220ff93a1519b3f281377cda9d
chaithanya <chaithanya1812@gmail.com>
2025-01-02 10:34:52 +0530

    sample werf1

---
+++ Day-30/terraform/list-loops/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "instances" {
+  default = ["db","web","app"]
+}
+resource "aws_instance" "main" {
+  count = length(var.instances)
+  ami = "ami-01816d07b1128cd2d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
