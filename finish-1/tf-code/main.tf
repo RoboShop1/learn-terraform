298dd14f9a59940256766afc550c0246cb677895
chaithanya <chaithanya1812@gmail.com>
2025-01-03 23:55:35 +0530

    pod scheduling

--- finish-1/tf-code/main.tf
+++ finish-1/tf-code/main.tf
@@ -51,6 +51,35 @@ resource "aws_iam_group" "developers" {
 }
 
 
+resource "aws_iam_group_policy" "my_developer_policy" {
+  name  = "my_developer_policy"
+  group = aws_iam_group.developers.name
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "sts:GetSessionToken",
+          "sts:DecodeAuthorizationMessage",
+          "sts:GetAccessKeyInfo",
+          "sts:GetCallerIdentity",
+          "sts:GetServiceBearerToken"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "sts:*",
+        "Resource": "arn:aws:iam::339712959230:role/arn:aws:iam::339712959230:role/eks_role_admin"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_group" "qa" {
   name = "qa"
 }
