3deb133444716e27c643dde4cf254ef1c0e052bc
chaithanya <chaithanya1812@gmail.com>
2025-01-03 22:26:23 +0530

    sample werf1

--- k8s-terraform/eks/main.tf
+++ k8s-terraform/eks/main.tf
@@ -38,7 +38,7 @@ resource "aws_eks_node_group" "dev-eks-public-nodegroup" {
   instance_types  = ["t3.small"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 2
     min_size     = 1
   }
@@ -123,4 +123,38 @@ resource "aws_eks_addon" "vpc-cni" {
 
 }
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
