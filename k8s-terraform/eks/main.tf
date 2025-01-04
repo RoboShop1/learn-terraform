2a13f2bfe5dcaeb7ac057fe70b0280c0ad5530ae
chaithanya <chaithanya1812@gmail.com>
2025-01-05 00:21:33 +0530

    pod scheduling

--- k8s-terraform/eks/main.tf
+++ k8s-terraform/eks/main.tf
@@ -128,10 +128,10 @@ resource "aws_eks_addon" "vpc-cni" {
 
 
 
-resource "aws_eks_addon" "example" {
+resource "aws_eks_addon" "vpc-cni" {
   cluster_name                = aws_eks_cluster.dev-eks.name
-  addon_name                  = "coredns"
-  addon_version               = "v1.10.1-eksbuild.1"
+  addon_name                  = "vpc-cni"
+  addon_version               = "v1.18.3-eksbuild.3"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
