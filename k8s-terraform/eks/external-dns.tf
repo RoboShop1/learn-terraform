784929dae28a3888127b748e46291a8b1513fb12
chaithanya <chaithanya1812@gmail.com>
2025-01-08 00:01:41 +0530

    pod scheduling

--- k8s-terraform/eks/external-dns.tf
+++ k8s-terraform/eks/external-dns.tf
@@ -1,64 +1,87 @@
-#data "aws_iam_policy_document" "eks-route-assume-role" {
-#  statement {
-#    effect = "Allow"
+# resource "aws_iam_policy" "sample" {
+#   name        = "${var.env}-node-externalDNS"
+#   path        = "/"
+#   description = "${var.env}-node-externalDNS"
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": [
+#         "route53:ChangeResourceRecordSets"
+#       ],
+#       "Resource": [
+#         "arn:aws:route53:::hostedzone/*"
+#       ]
+#     },
+#     {
+#       "Effect": "Allow",
+#       "Action": [
+#         "route53:ListHostedZones",
+#         "route53:ListResourceRecordSets",
+#         "route53:ListTagsForResource"
+#       ],
+#       "Resource": [
+#         "*"
+#       ]
+#     }
+#   ]
+# }
+# EOF
+# }
 #
-#    principals {
-#      type        = "Service"
-#      identifiers = ["pods.eks.amazonaws.com"]
-#    }
+# variable "env" {
+#   default = "prod"
+# }
 #
-#    actions = [
-#      "sts:AssumeRole",
-#      "sts:TagSession"
-#    ]
-#  }
-#}
 #
-#resource "aws_iam_role" "eks-route-role" {
-#  name               = "aws_elb_role"
-#  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-#}
 #
-#resource "aws_iam_role_policy" "eks-route-policy" {
-#  name = "test_policy"
-#  role = aws_iam_role.eks-route-role.id
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
 #
-#  policy = jsonencode({
-#    "Version": "2012-10-17",
-#    "Statement": [
-#      {
-#        "Effect": "Allow",
-#        "Action": [
-#          "route53:ChangeResourceRecordSets"
-#        ],
-#        "Resource": [
-#          "arn:aws:route53:::hostedzone/*"
-#        ]
-#      },
-#      {
-#        "Effect": "Allow",
-#        "Action": [
-#          "route53:ListHostedZones",
-#          "route53:ListResourceRecordSets",
-#          "route53:ListTagsForResource"
-#        ],
-#        "Resource": [
-#          "*"
-#        ]
-#      }
-#    ]
-#  })
-#}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["pods.eks.amazonaws.com"]
+#     }
 #
-#resource "aws_eks_pod_identity_association" "external-pod-identity" {
-#  cluster_name    = aws_eks_cluster.dev-eks.name
-#  namespace       = "default"
-#  service_account = "external-sa"
-#  role_arn        = aws_iam_role.eks-route-role.arn
-#}
+#     actions = [
+#       "sts:AssumeRole",
+#       "sts:TagSession"
+#     ]
+#   }
+# }
 #
-#resource "helm_release" "external-dns" {
-#  depends_on = [aws_eks_pod_identity_association.external-pod-identity]
+#
+# resource "aws_iam_role" "example" {
+#   name               = "sample"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
+#
+#
+# resource "aws_iam_role_policy_attachment" "example_s3" {
+#   policy_arn = aws_iam_policy.sample.arn
+#   role       = aws_iam_role.example.name
+# }
+#
+# resource "aws_eks_pod_identity_association" "example" {
+#   cluster_name    = "dev-eks"
+#   namespace       = "default"
+#   service_account = "external-dns"
+#   role_arn        = aws_iam_role.example.arn
+# }
+#
+#
+# provider "helm" {
+#   kubernetes {
+#     config_path = "~/.kube/config"
+#   }
+# }
+#
+#
+#
+#  resource "helm_release" "external-dns" {
 #  name       = "external-dns"
 #  repository = "https://kubernetes-sigs.github.io/external-dns/"
 #  chart      = "external-dns"
@@ -67,6 +90,6 @@
 #
 #  set {
 #    name  = "serviceAccount.name"
-#    value = "external-sa"
+#    value = "external-dns1"
 #  }
-#}
+# }
