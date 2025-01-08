acc13177a932aad10b208e1b087070f23c524793
chaithanya <chaithanya1812@gmail.com>
2025-01-08 10:48:34 +0530

    pod scheduling

--- k8s-terraform/eks/irsa.tf
+++ k8s-terraform/eks/irsa.tf
@@ -103,3 +103,42 @@ resource "aws_iam_role_policy_attachment" "irsa_iam_role_policy_attach2" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53ReadOnlyAccess"
   role       = aws_iam_role.eks-cluster-route53.name
 }
+
+resource "aws_eks_addon" "eks-pod-identity-agent" {
+  depends_on = [aws_eks_node_group.dev-eks-public-nodegroup]
+
+  cluster_name                = aws_eks_cluster.dev-eks.name
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = "v1.36.0-eksbuild.1"
+  resolve_conflicts_on_update = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
