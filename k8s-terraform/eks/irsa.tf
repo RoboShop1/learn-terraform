a210690fac17ae7a0c675c608219d2eab2ddb150
chaithanya <chaithanya1812@gmail.com>
2025-01-08 20:23:15 +0530

    pod scheduling

--- k8s-terraform/eks/irsa.tf
+++ k8s-terraform/eks/irsa.tf
@@ -179,8 +179,15 @@ resource "aws_eks_identity_provider_config" "oidc" {
 }
 
 
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+  url             = aws_eks_cluster.dev-eks.identity[0].oidc[0].issuer
 
-
+  tags = {
+      Name = "dev-eks-irsa"
+  }
+}
 
 
 
