f8d522f7560febf03474adf87b92acfa474f9163
chaithanya <chaithanya1812@gmail.com>
2025-01-16 21:25:59 +0530

    tf code

--- Day-41/main.tf
+++ Day-41/main.tf
@@ -9,16 +9,16 @@ module "vpc" {
 
 variable "subnets" {
   default = {
-    public_subnets = {
+    public-subnets = {
       cidr_blocks = ["10.0.1.0/24","10.0.2.0/24"]
     }
-    web_subnets = {
+    web-subnets = {
       cidr_blocks = ["10.0.3.0/24","10.0.4.0/24"]
     }
-    app_subnets = {
+    app-subnets = {
       cidr_blocks = ["10.0.5.0/24","10.0.6.0/24"]
     }
-    db_subnets = {
+    db-subnets = {
       cidr_blocks = ["10.0.7.0/24","10.0.8.0/24"]
     }
   }
