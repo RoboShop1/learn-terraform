c99e0cdb5ac3d2ee2b97e2e97643fc18af99a2d8
chaithanya <chaithanya1812@gmail.com>
2025-01-19 12:24:06 +0530

    tf code

--- Day-32/vpc/main.tf
+++ Day-32/vpc/main.tf
@@ -25,9 +25,9 @@ output "out_sub" {
   value = module.subnets
 }
 
-output "sample" {
-  value = { for i,k in  module.subnets: i => { values(k.subnets).*.id } }
-}
+# output "sample" {
+#   value = { for i,k in  module.subnets: i => { values(k.subnets).*.id } }
+# }
 # output "out_sub" {
 #   value ={for i,k in module.subnets:  i => values(k.subnets).*.id  }
 # }
