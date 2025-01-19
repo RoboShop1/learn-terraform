a7e3c7c72e0d80ed0c6473aee51cbe8ed0697b56
chaithanya <chaithanya1812@gmail.com>
2025-01-19 16:33:10 +0530

    tf code

--- Day-32/vpc/subnets/outputs.tf
+++ Day-32/vpc/subnets/outputs.tf
@@ -5,3 +5,12 @@
 # output "route_tables_ids" {
 #   value = { for i,k in aws_route_table.main: i => k.id }
 # }
+
+
+output "subnet_ids" {
+  value = aws_subnet.main
+}
+
+output "route_table_ids" {
+  value = aws_route_table.main
+}
