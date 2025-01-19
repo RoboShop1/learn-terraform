1d3fd1c3ec460e5a2cac9dc189419d10107a5f1c
chaithanya <chaithanya1812@gmail.com>
2025-01-19 11:27:47 +0530

    tf code

--- Day-32/vpc/subnets/main.tf
+++ Day-32/vpc/subnets/main.tf
@@ -9,6 +9,16 @@ resource "aws_subnet" "main" {
   }
 }
 
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "${var.env}-${each.key}-rt"
+  }
+}
+
+
 variable "env" {}
 variable "vpc_id" {}
 variable "subnets" {}
