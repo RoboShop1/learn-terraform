96760cb373a8dcbd6adc39e8c837e2e770744060
chaithanya <chaithanya1812@gmail.com>
2025-01-19 17:23:05 +0530

    tf code

--- Day-32/vpc/main.tf
+++ Day-32/vpc/main.tf
@@ -53,7 +53,7 @@ resource "aws_nat_gateway" "main" {
 
 
 output "subnets" {
-  value = { for i,k in module.subnets:  i => k.subnet_ids if i != "public" }
+  value = {for m,n in { for i,k in module.subnets:  i => k.subnet_ids if i != "public" }: m => keys(n) }
 }
 
 
