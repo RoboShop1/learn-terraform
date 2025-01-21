92e76904dabec4062d46289e50215ef5dbe63b54
chaithanya <chaithanya1812@gmail.com>
2025-01-22 00:24:52 +0530

    tf code

--- k8s-terraform/eks/irsa1.tf
+++ k8s-terraform/eks/irsa1.tf
@@ -10,4 +10,38 @@ resource "aws_iam_openid_connect_provider" "default" {
   }
 }
 
+
+resource "aws_iam_role" "s33_role" {
+  name = "s33_role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Federated": "arn:aws:iam::339712959230:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388"
+        },
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+          "StringEquals": {
+            "oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388:aud": "sts.amazonaws.com",
+            "oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388:aud": "system:serviceaccount:default:dev-sa"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    tag-key = "s3-value"
+  }
+}
+
+
+resource "aws_iam_role_policy_attachment" "s3-attach" {
+  role       = aws_iam_role.s33_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
 # kubergrunt  eks oidc-thumbprint  --issuer-url aws_eks_cluster.cluster.identity.0.oidc.0.issuer
