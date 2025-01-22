89a99b186a24748dc7225a24de417327e468194a
chaithanya <chaithanya1812@gmail.com>
2025-01-22 23:43:21 +0530

    tf code

--- packer/main.tf
+++ packer/main.tf
@@ -13,7 +13,7 @@ output "name" {
 resource "aws_instance" "main" {
   count        = 3
   instance_type = "t3.small"
-  ami = "ami-09f49ac1793f38340"
+  ami = "ami-0df8c184d5f6ae949"
   key_name = "nvirginia"
 
   lifecycle {
