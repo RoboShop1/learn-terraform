e156308b6fd412bbd21940a77d8f769b285db956
chaithanya <chaithanya1812@gmail.com>
2025-01-22 21:03:16 +0530

    tf code

--- packer/setup.sh
+++ packer/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 sudo dnf install nginx -y
+sudo rm -rf /usr/share/nginx/html/*
+sudo echo "<h1> This is Index chaitu </h1>"  > /usr/share/nginx/html/index.html
 sudo systemctl start nginx
 sudo echo "This is tmp" > /tmp/1.txt
 sudo dnf install git -y
