a98641998d4d26b55f64d7f5063d98b2d798a4e9
chaithanya <chaithanya1812@gmail.com>
2025-01-22 20:46:34 +0530

    tf code

--- packer/main.tf
+++ packer/main.tf
@@ -1,5 +1,4 @@
 data "aws_ami" "example" {
-
   most_recent      = true
   name_regex       = "learn-terraform-packer-v1"
   owners           = ["self"]
@@ -10,9 +9,13 @@ output "name" {
   value = data.aws_ami.example
 }
 
-#
-#
-# resource "aws_instance" "main" {
-#   instance_type = "t2.micro"
-#   ami = ""
-# }
+
+resource "aws_instance" "main" {
+  instance_type = "t2.micro"
+  ami = data.aws_ami.example.image_id
+  key_name = "nvirginia"
+
+  tags  = {
+    Name = "sample-tag"
+  }
+}
