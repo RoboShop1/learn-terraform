662cf59f6babb1e68f69129a08391ff3b38d6793
chaithanya <chaithanya1812@gmail.com>
2025-01-22 21:18:45 +0530

    tf code

--- packer/main.tf
+++ packer/main.tf
@@ -13,8 +13,12 @@ output "name" {
 resource "aws_instance" "main" {
   count        = 3
   instance_type = "t3.small"
-  ami = data.aws_ami.example.image_id
+  ami = "ami-0df8c184d5f6ae949"
   key_name = "nvirginia"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags  = {
     Name = "sample-tag-${count.index}"
