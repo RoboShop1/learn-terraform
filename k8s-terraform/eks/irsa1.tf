6f715b7ee75024ec848273062971185f7f7fd063
chaithanya <chaithanya1812@gmail.com>
2025-01-26 19:40:45 +0530

    k8s

--- k8s-terraform/eks/irsa1.tf
+++ k8s-terraform/eks/irsa1.tf
@@ -1,48 +1,48 @@
-resource "aws_iam_openid_connect_provider" "default" {
-  url = aws_eks_cluster.dev-eks.identity[0].oidc[0].issuer
-
-  client_id_list  = ["sts.amazonaws.com"]
-
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
-
-  tags = {
-    Name = "eks-oidc"
-  }
-}
-
-
-resource "aws_iam_role" "s33_role" {
-  name = "s33_role"
-
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Federated": "arn:aws:iam::339712959230:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388"
-        },
-        "Action": "sts:AssumeRoleWithWebIdentity",
-        "Condition": {
-          "StringEquals": {
-            "oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388:aud": "sts.amazonaws.com",
-            "oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388:sub": "system:serviceaccount:kube-system:dev-sa"
-          }
-        }
-      }
-    ]
-  })
-
-  tags = {
-    tag-key = "s3-value"
-  }
-}
-
-
-resource "aws_iam_role_policy_attachment" "s3-attach" {
-  role       = aws_iam_role.s33_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
+# resource "aws_iam_openid_connect_provider" "default" {
+#   url = aws_eks_cluster.dev-eks.identity[0].oidc[0].issuer
+#
+#   client_id_list  = ["sts.amazonaws.com"]
+#
+#   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+#
+#   tags = {
+#     Name = "eks-oidc"
+#   }
+# }
+#
+#
+# resource "aws_iam_role" "s33_role" {
+#   name = "s33_role"
+#
+#   assume_role_policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#       {
+#         "Effect": "Allow",
+#         "Principal": {
+#           "Federated": "arn:aws:iam::339712959230:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388"
+#         },
+#         "Action": "sts:AssumeRoleWithWebIdentity",
+#         "Condition": {
+#           "StringEquals": {
+#             "oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388:aud": "sts.amazonaws.com",
+#             "oidc.eks.us-east-1.amazonaws.com/id/62E9F3B44BB1C4FC1B0769CD30BEC388:sub": "system:serviceaccount:kube-system:dev-sa"
+#           }
+#         }
+#       }
+#     ]
+#   })
+#
+#   tags = {
+#     tag-key = "s3-value"
+#   }
+# }
+#
+#
+# resource "aws_iam_role_policy_attachment" "s3-attach" {
+#   role       = aws_iam_role.s33_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+# }
 
 
 
