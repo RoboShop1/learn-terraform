d288406ed19f559e1aa78c870bfff2a7db053327
chaithanya <chaithanya1812@gmail.com>
2025-01-28 13:08:30 +0530

    k8s

--- Day-322/outputs.tf
+++ Day-322/outputs.tf
@@ -7,14 +7,14 @@ output "app_subnets" {
   value = { for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" }
 }
 
-output "values" {
-  value = values({ for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" })
-}
-
-
-# resource "aws_instance" "main" {
-#   for_each = values({ for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" })
-#   ami           = "ami-0b4f379183e5706b9"
-#   instance_type = "t2.micro"
-#   subnet_id     = each.value
+# output "values" {
+#   value = values({ for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" })
 # }
+
+
+resource "aws_instance" "main" {
+  for_each = toset(values({ for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" }))
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  subnet_id     = each.value
+}
