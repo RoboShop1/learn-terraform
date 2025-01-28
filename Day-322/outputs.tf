fd397ddc7864e17ef3ca24a551dfa535bacd399f
chaithanya <chaithanya1812@gmail.com>
2025-01-28 13:05:29 +0530

    k8s

--- Day-322/outputs.tf
+++ Day-322/outputs.tf
@@ -9,7 +9,7 @@ output "app_subnets" {
 
 
 resource "aws_instance" "main" {
-  for_each = { for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" }
+  for_each = values({ for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" })
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
   subnet_id     = each.value
