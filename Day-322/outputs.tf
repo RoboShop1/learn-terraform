553696045669e326d359a7724830c4289b918a25
chaithanya <chaithanya1812@gmail.com>
2025-01-28 13:03:32 +0530

    k8s

--- Day-322/outputs.tf
+++ Day-322/outputs.tf
@@ -1,7 +1,16 @@
+
 output "vpc" {
   value = module.vpc
 }
 
 output "app_subnets" {
   value = { for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" }
+}
+
+
+resource "aws_instance" "main" {
+  for_each = { for i,k in lookup(lookup(module.vpc,"dev",null),"subnets_main",null): i => k if i == "app" }
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  subnet_id     = each.value
 }
