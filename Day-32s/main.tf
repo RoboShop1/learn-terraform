cb92b94fa5e1883743e0fca37fedf5d7ff250cdd
chaithanya <chaithanya1812@gmail.com>
2025-02-13 15:43:00 +0530

    tf-vpc

--- Day-32s/main.tf
+++ Day-32s/main.tf
@@ -32,6 +32,27 @@ variable "vpc" {
         }
       }
     }
+    prod = {
+      vpc_cidr_block = "10.2.0.0/16"
+      subnets        = {
+        public = {
+          public1 = {cidr_block = "10.2.1.0/24" , az = "us-east-1a"}
+          public2 = {cidr_block = "10.2.2.0/24" , az = "us-east-1b"}
+        }
+        web = {
+          web1 = {cidr_block = "10.2.3.0/24" , az = "us-east-1a"}
+          web2 = {cidr_block = "10.2.4.0/24" , az = "us-east-1b"}
+        }
+        app = {
+          app1 = {cidr_block = "10.2.5.0/24" , az = "us-east-1a"}
+          app2 = {cidr_block = "10.2.6.0/24" , az = "us-east-1b"}
+        }
+        db = {
+          db1 = {cidr_block = "10.2.7.0/24" , az = "us-east-1a"}
+          db2 = {cidr_block = "10.2.8.0/24" , az = "us-east-1b"}
+        }
+      }
+    }
   }
 }
 
