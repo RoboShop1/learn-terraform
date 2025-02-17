33ffa8b3fa4883f58c7c839f72313d60aa4b44b5
chaithanya <chaithanya1812@gmail.com>
2025-02-17 22:32:41 +0530

    probe

---
+++ Nice-32s/main.tf
@@ -0,0 +1,16 @@
+resource "aws_launch_template" "main" {
+  image_id      = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+
+  placement {
+    availability_zone = "us-east-1b"
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "sample"
+    }
+  }
+}
