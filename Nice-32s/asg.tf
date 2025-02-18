d7740dcfa2f9cd110ba520322617cb83572c6305
chaithanya <chaithanya1812@gmail.com>
2025-02-18 11:31:28 +0530

    probe

--- Nice-32s/asg.tf
+++ Nice-32s/asg.tf
@@ -21,28 +21,29 @@ resource "aws_autoscaling_group" "bar" {
 
 
 
-resource "aws_instance" "main" {
-  ami = "ami-0b4f379183e5706b9"
-
-  instance_type = "t3.small"
-
-  root_block_device {
-    volume_size = 20
-    encrypted = true
-    kms_key_id = data.aws_kms_key.main.id
-
-  }
-
-  tags = {
-    Name = "demo-encrypt"
-  }
-
-}
-
-data "aws_kms_key" "main" {
-  key_id = "alias/my-secret-key"
-}
-
-output "all" {
-  value = data.aws_kms_key.main
-}
+# resource "aws_instance" "main" {
+#   ami = "ami-0b4f379183e5706b9"
+#
+#   instance_type = "t3.small"
+#
+#   root_block_device {
+#     volume_size = 20
+#     encrypted = true
+#     kms_key_id = data.aws_kms_key.main.id
+#
+#   }
+#
+#   tags = {
+#     Name = "demo-encrypt"
+#   }
+#
+# }
+#
+#
+# data "aws_kms_key" "main" {
+#   key_id = "alias/my-secret-key"
+# }
+#
+# output "all" {
+#   value = data.aws_kms_key.main
+# }
