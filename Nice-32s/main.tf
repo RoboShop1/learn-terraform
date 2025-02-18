d08081bd8ff83e12df1e151e42791cecf49b4547
chaithanya <chaithanya1812@gmail.com>
2025-02-18 10:29:01 +0530

    probe

--- Nice-32s/main.tf
+++ Nice-32s/main.tf
@@ -4,6 +4,14 @@ resource "aws_launch_template" "main" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0665a56c7cd09a0e0"]
 
+  block_device_mappings {
+    device_name = "/dev/sdf"
+
+    ebs {
+      volume_size = 5
+    }
+  }
+
   placement {
     availability_zone = "us-east-1b"
   }
