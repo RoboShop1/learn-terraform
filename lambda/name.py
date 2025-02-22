81a040ac57b09d722c3e5c9a8564a8a81e9595ff
chaithanya <chaithanya1812@gmail.com>
2025-02-22 11:40:56 +0530

    lambda

--- lambda/name.py
+++ lambda/name.py
@@ -2,8 +2,10 @@ import json
 import boto3
 
 def lambda_handler(event,context):
+    print("====================")
     print("This is chaithanya")
     print("This is sample example")
+    print("=======================")
     return {
         'statusCode': 200
         'body': json.dumps('Hello from Lambda')
