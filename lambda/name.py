d56ea5915fb92f2dfc0e5acd2a1fc3fe63a170ed
chaithanya <chaithanya1812@gmail.com>
2025-03-01 17:36:42 +0530

    lambda

--- lambda/name.py
+++ lambda/name.py
@@ -10,8 +10,14 @@ def lambda_handler(event,context):
 
     client = boto3.client('ec2')
     ids = ['i-0817ad560e194335d','i-078667ba3a54220bd']
-    reponse = client.describe_instances( InstanceIds=ids )
-    print(reponse)
+    response = client.describe_instances( InstanceIds=ids )
+
+    for reservation in reponse["Reservations"]:
+        for instance in reservation["Instances"]:
+            print('====================')
+            print(f" {instance["InstanceId"]} == {instance["State"]["Name"]})
+            print('======================')
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda')
