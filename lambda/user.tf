bcb5679cdc1dfb8ee9689698714db0ec1ead3722
chaithanya <chaithanya1812@gmail.com>
2025-03-01 21:12:37 +0530

    lambda

--- lambda/user.tf
+++ lambda/user.tf
@@ -20,3 +20,11 @@ output "secret_access_key" {
   value = aws_iam_access_key.my_user_key.secret
   sensitive = true
 }
+
+
+resource "aws_lambda_permission" "allow_specific_user" {
+  statement_id  = "AllowSpecificUser"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = aws_iam_user.lb.arn  # Replace with actual IAM user ARN
+}
