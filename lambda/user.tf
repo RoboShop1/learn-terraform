128005eb4a34ae9876dc2848f55c73bd9c738b2d
chaithanya <chaithanya1812@gmail.com>
2025-03-01 21:03:04 +0530

    lambda

---
+++ lambda/user.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_user" "lb" {
+  name = "invoke"
+
+  tags = {
+    Name = "invoke"
+  }
+}
+
+resource "aws_iam_access_key" "my_user_key" {
+  user = aws_iam_user.lb.name
+}
+
+# Output Access Key (Visible in Console)
+output "access_key_id" {
+  value = aws_iam_access_key.my_user_key.id
+}
+
+# Output Secret Key (Sensitive - Hidden in Console)
+output "secret_access_key" {
+  value = aws_iam_access_key.my_user_key.secret
+
+}
