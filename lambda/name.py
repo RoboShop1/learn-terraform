389b4437bb7a0d39c76d5edc9b9beab998f53dfb
chaithanya <chaithanya1812@gmail.com>
2025-03-01 17:15:27 +0530

    lambda

--- lambda/name.py
+++ lambda/name.py
@@ -4,8 +4,12 @@ import os
 
 
 def lambda_handler(event,context):
+    print("==========")
+    print(event)
+    print("==========")
+
     client = boto3.client('ec2')
-    ids = ['i-0817ad560e194335d']
+    ids = ['i-0817ad560e194335d','i-078667ba3a54220bd']
     reponse = client.describe_instances( InstanceIds=ids )
     print(reponse)
     return {
