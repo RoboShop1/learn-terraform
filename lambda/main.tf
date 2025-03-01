d3fcb505c848ab72d3c81e10acc3f2d94fa62b87
chaithanya <chaithanya1812@gmail.com>
2025-03-01 21:15:31 +0530

    lambda

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,79 +1,79 @@
-
-resource "aws_iam_role" "lambda_role" {
-  name = "tf_lambda_role1"
-  assume_role_policy =<<EOT
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-EOT
-}
-
-
-resource "aws_iam_policy" "lambda_ec2_policy" {
-  name        = "lambda_ec2_describe_policy"
-  description = "Allow Lambda to describe EC2 instances"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = "ec2:DescribeInstances"
-        Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": "logs:CreateLogGroup",
-        "Resource": "arn:aws:logs:us-east-1:339712959230:*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource": "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
-  policy_arn = aws_iam_policy.lambda_ec2_policy.arn
-  role       = aws_iam_role.lambda_role.name
-}
-
-data "archive_file" "sample" {
-  source_file = "name.py"
-  output_path = "name.zip"
-  type        = "zip"
-}
-
-
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "name.zip"
-  function_name = "sample2"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "name.lambda_handler"
-
-  source_code_hash = data.archive_file.sample.output_base64sha256
-
-  runtime = "python3.11"
-
-  timeout = 120
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
-}
+#
+# resource "aws_iam_role" "lambda_role" {
+#   name = "tf_lambda_role1"
+#   assume_role_policy =<<EOT
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Principal": {
+#                 "Service": "lambda.amazonaws.com"
+#             },
+#             "Action": "sts:AssumeRole"
+#         }
+#     ]
+# }
+# EOT
+# }
+#
+#
+# resource "aws_iam_policy" "lambda_ec2_policy" {
+#   name        = "lambda_ec2_describe_policy"
+#   description = "Allow Lambda to describe EC2 instances"
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action   = "ec2:DescribeInstances"
+#         Effect   = "Allow"
+#         Resource = "*"
+#       },
+#       {
+#         "Effect": "Allow",
+#         "Action": "logs:CreateLogGroup",
+#         "Resource": "arn:aws:logs:us-east-1:339712959230:*"
+#       },
+#       {
+#         "Effect": "Allow",
+#         "Action": [
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents"
+#         ],
+#         "Resource": "*"
+#       }
+#     ]
+#   })
+# }
+#
+# resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+#   policy_arn = aws_iam_policy.lambda_ec2_policy.arn
+#   role       = aws_iam_role.lambda_role.name
+# }
+#
+# data "archive_file" "sample" {
+#   source_file = "name.py"
+#   output_path = "name.zip"
+#   type        = "zip"
+# }
+#
+#
+# resource "aws_lambda_function" "test_lambda" {
+#   filename      = "name.zip"
+#   function_name = "sample2"
+#   role          = aws_iam_role.lambda_role.arn
+#   handler       = "name.lambda_handler"
+#
+#   source_code_hash = data.archive_file.sample.output_base64sha256
+#
+#   runtime = "python3.11"
+#
+#   timeout = 120
+#
+#   environment {
+#     variables = {
+#       foo = "bar"
+#     }
+#   }
+# }
--- lambda/user.tf
+++ lambda/user.tf
@@ -22,9 +22,16 @@ output "secret_access_key" {
 }
 
 
-resource "aws_lambda_permission" "allow_specific_user" {
-  statement_id  = "AllowSpecificUser"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.test_lambda.function_name
-  principal     = aws_iam_user.lb.arn  # Replace with actual IAM user ARN
+provider "local" {}
+
+resource "local_file" "example" {
+  filename = "/tmp/example.txt"
+  content  = aws_iam_access_key.my_user_key.secret
 }
+
+# resource "aws_lambda_permission" "allow_specific_user" {
+#   statement_id  = "AllowSpecificUser"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.test_lambda.function_name
+#   principal     = aws_iam_user.lb.arn  # Replace with actual IAM user ARN
+# }
