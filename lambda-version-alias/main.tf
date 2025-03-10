2cfe976e7b148f551ddab45310abe01b3d3da23e
chaithanya <chaithanya1812@gmail.com>
2025-03-10 12:12:43 +0530

    aws-lambda

--- lambda-version-alias/main.tf
+++ lambda-version-alias/main.tf
@@ -4,11 +4,11 @@ data "aws_lambda_function" "existing" {
   function_name = "sample"
 }
 
-
-output "all" {
-  value = data.aws_lambda_function.existing
-}
 resource "aws_lambda_function_url" "test_live" {
   function_name      = data.aws_lambda_function.existing.function_name
   authorization_type = "AWS_IAM"
+}
+
+output "url" {
+  value = aws_lambda_function_url.test_live.function_url
 }
