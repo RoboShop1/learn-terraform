bd0f49482f944936dbd1d3cde60526d810415dca
chaithanya <chaithanya1812@gmail.com>
2025-03-10 12:19:25 +0530

    aws-lambda

--- lambda-version-alias/main.tf
+++ lambda-version-alias/main.tf
@@ -12,3 +12,12 @@ resource "aws_lambda_function_url" "test_live" {
 output "url" {
   value = aws_lambda_function_url.test_live.function_url
 }
+
+
+resource "aws_lambda_permission" "allow_user" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunctionUrl"
+  function_name = data.aws_lambda_function.existing.function_name
+  principal     = "arn:aws:iam::339712959230:user/key-user"
+  function_url_auth_type = "AWS_IAM"
+}
