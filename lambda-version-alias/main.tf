a93494fbfaa3341ec0c6380754ff920aeb2b89a8
chaithanya <chaithanya1812@gmail.com>
2025-03-10 12:09:49 +0530

    aws-lambda

--- lambda-version-alias/main.tf
+++ lambda-version-alias/main.tf
@@ -1,7 +1,15 @@
 
 
-resource "aws_lambda_function_url" "test_live" {
-  function_name      = "sample"
-  qualifier          = "my_alias"
-  authorization_type = "AWS_IAM"
+data "aws_lambda_function" "existing" {
+  function_name = "sample"
 }
+
+
+output "all" {
+  value = data.aws_lambda_function.existing
+}
+# resource "aws_lambda_function_url" "test_live" {
+#   function_name      = "sample"
+#   qualifier          = "my_alias"
+#   authorization_type = "AWS_IAM"
+# }
