acbbc5531f50b28bd2d5d62479f855d6217aa190
chaithanya <chaithanya1812@gmail.com>
2025-04-30 07:55:08 +0530

    ss

---
+++ vault1/main.tf
@@ -0,0 +1,16 @@
+provider "vault" {
+  address = "http://44.197.238.225:8200/"
+  skip_tls_verify = true
+  token = var.token
+}
+
+variable "token" {}
+
+data "vault_kv_secret" "secret_data" {
+  path = "k8s/data/cart"
+}
+
+
+output "data1" {
+  value = data.vault_kv_secret.secret_data
+}
