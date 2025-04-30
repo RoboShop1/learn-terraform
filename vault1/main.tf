5c72785d6812a4d4bc9570730907117731aa4ff5
chaithanya <chaithanya1812@gmail.com>
2025-04-30 08:08:02 +0530

    ss

--- vault1/main.tf
+++ vault1/main.tf
@@ -13,7 +13,7 @@ data "vault_kv_secret" "secret_data" {
 
 
 output "data1" {
-  value = data.vault_kv_secret.secret_data.data_json["name1"]
+  value = data.vault_kv_secret.secret_data.data_json
   sensitive = true
 }
 
