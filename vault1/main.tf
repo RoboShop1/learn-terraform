df700edb6722637c56c341eac50a692390698bcb
chaithanya <chaithanya1812@gmail.com>
2025-04-30 08:00:07 +0530

    ss

--- vault1/main.tf
+++ vault1/main.tf
@@ -1,3 +1,4 @@
+
 provider "vault" {
   address = "http://44.197.238.225:8200/"
   skip_tls_verify = true
@@ -14,4 +15,12 @@ data "vault_kv_secret" "secret_data" {
 output "data1" {
   value = data.vault_kv_secret.secret_data
   sensitive = true
+}
+
+resource "null_resource" "main" {
+  provisioner "local-exec" {
+    command =<<EOT
+echo ${data.vault_kv_secret.secret_data.data} > /tmp/3.txt
+EOT
+  }
 }
